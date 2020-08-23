31c2fe3a578fa8f63172bfac0d8be00c02c8890e
Damián Culotta <damianculotta@gmail.com>
2020-08-23 19:42:01 -0300

    Fixed Regions ISO 3166-2 code.

--- Setup/Patch/Data/AddRegions.php
+++ Setup/Patch/Data/AddRegions.php
@@ -46,29 +46,33 @@ class AddRegions implements DataPatchInterface, PatchRevertableInterface
          * Fill table directory/country_region_name for en_US locale
          */
         $data = [
-            [self::COUNTRY_CODE, 'AR', 'Artigas'],
-            [self::COUNTRY_CODE, 'CA', 'Canelones'],
-            [self::COUNTRY_CODE, 'CL', 'Cerro Largo'],
-            [self::COUNTRY_CODE, 'CO', 'Colonia'],
-            [self::COUNTRY_CODE, 'DU', 'Durazno'],
-            [self::COUNTRY_CODE, 'FS', 'Flores'],
-            [self::COUNTRY_CODE, 'FD', 'Florida'],
-            [self::COUNTRY_CODE, 'LA', 'Lavalleja'],
-            [self::COUNTRY_CODE, 'MA', 'Maldonado'],
-            [self::COUNTRY_CODE, 'MO', 'Montevideo'],
-            [self::COUNTRY_CODE, 'PA', 'Paysandu'],
-            [self::COUNTRY_CODE, 'RN', 'Río Negro'],
-            [self::COUNTRY_CODE, 'RV', 'Rivera'],
-            [self::COUNTRY_CODE, 'RO', 'Rocha'],
-            [self::COUNTRY_CODE, 'SA', 'Salto'],
-            [self::COUNTRY_CODE, 'SJ', 'San José'],
-            [self::COUNTRY_CODE, 'SO', 'Soriano'],
-            [self::COUNTRY_CODE, 'TA', 'Tacuarembó'],
-            [self::COUNTRY_CODE, 'TT', 'Treinta y Tres']
+            [self::COUNTRY_CODE . '-AR', 'Artigas'],
+            [self::COUNTRY_CODE . '-CA', 'Canelones'],
+            [self::COUNTRY_CODE . '-CL', 'Cerro Largo'],
+            [self::COUNTRY_CODE . '-CO', 'Colonia'],
+            [self::COUNTRY_CODE . '-DU', 'Durazno'],
+            [self::COUNTRY_CODE . '-FS', 'Flores'],
+            [self::COUNTRY_CODE . '-FD', 'Florida'],
+            [self::COUNTRY_CODE . '-LA', 'Lavalleja'],
+            [self::COUNTRY_CODE . '-MA', 'Maldonado'],
+            [self::COUNTRY_CODE . '-MO', 'Montevideo'],
+            [self::COUNTRY_CODE . '-PA', 'Paysandu'],
+            [self::COUNTRY_CODE . '-RN', 'Río Negro'],
+            [self::COUNTRY_CODE . '-RV', 'Rivera'],
+            [self::COUNTRY_CODE . '-RO', 'Rocha'],
+            [self::COUNTRY_CODE . '-SA', 'Salto'],
+            [self::COUNTRY_CODE . '-SJ', 'San José'],
+            [self::COUNTRY_CODE . '-SO', 'Soriano'],
+            [self::COUNTRY_CODE . '-TA', 'Tacuarembó'],
+            [self::COUNTRY_CODE . '-TT', 'Treinta y Tres']
         ];
 
         foreach ($data as $row) {
-            $bind = ['country_id' => $row[0], 'code' => $row[1], 'default_name' => $row[2]];
+            $bind = [
+                'country_id' => self::COUNTRY_CODE,
+                'code' => $row[0],
+                'default_name' => $row[1]
+            ];
             $this->moduleDataSetup->getConnection()->insert(
                 $this->moduleDataSetup->getTable('directory_country_region'),
                 $bind
@@ -78,7 +82,7 @@ class AddRegions implements DataPatchInterface, PatchRevertableInterface
                 $this->moduleDataSetup->getTable('directory_country_region')
             );
 
-            $bind = ['locale' => 'en_US', 'region_id' => $regionId, 'name' => $row[2]];
+            $bind = ['locale' => 'en_US', 'region_id' => $regionId, 'name' => $row[1]];
             $this->moduleDataSetup->getConnection()->insert(
                 $this->moduleDataSetup->getTable('directory_country_region_name'),
                 $bind
